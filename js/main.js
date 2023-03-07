9c7add3534da035800d80172356a78eb5df9c357
Roman Petrenko <petrenkorv95@gmail.com>
2023-03-07 10:04:28 +0700

    add js

--- js/main.js
+++ js/main.js
@@ -8,4 +8,16 @@ $(function () {
             header.removeClass("scroll");
         }
     });
+
+    $("#form").on("submit", function (e) {
+        e.preventDefault();
+        $(this).hide();
+        $(this)[0].reset();
+        $("#thankyou").show();
+    });
+    $("#thankyou button").on("click", function (e) {
+        e.preventDefault();
+        $("#form").show();
+        $("#thankyou").hide();
+    });
 });
