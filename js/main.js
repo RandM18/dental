22ffeca60ff7e39a54130c8ba18b20c4053b27aa
Maxim <mladoshin@mail.ru>
2023-03-08 23:10:26 +0300

    add laptop parallax

--- js/main.js
+++ js/main.js
@@ -9,6 +9,24 @@ $(function () {
         }
     });
 
+    const header_title = $(".header-title");
+    const laptop = $(".laptop-inner");
+    $(window).scroll(function (e) {
+        let pos = $(window).scrollTop();
+        console.log(pos)
+        if (pos > 10) {
+            header_title.addClass("scroll");
+            laptop.addClass("scroll");
+            if(pos < 300){
+                laptop.css('top', `calc(50% + ${pos*0.65}px)`);
+            }
+        } else{
+            header_title.removeClass("scroll");
+            laptop.removeClass("scroll");
+            laptop.css('top', `0px`);
+        }
+    });
+
     $("#form").on("submit", function (e) {
         e.preventDefault();
         $(this).hide();
